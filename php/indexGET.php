9d77c87e428d5a26d481051b1781986b9d7b7534
David Macisso <dmacisso@gmail.com>
2016-10-07 15:25:05 -0400

    mysql pdfs

---
+++ php/indexGET.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+    <title>My First PHP Webpage</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+</head>
+
+<body>
+<div>
+    
+    <?php
+    
+    
+    if ($_GET["submit"]) {
+        
+    
+        if ($_GET["name"]) {
+        
+            echo "Your name is " . $_GET["name"];
+        
+        } else {
+            
+            echo "Please Enter Your Name";
+        }
+    }
+    ?>
+    
+ <form>
+     
+     <label for="name">Name: </label>
+     <input name="name" type="text" />
+     <input type="submit" name="submit" value="Submit Name" />
+    
+
+</form>
+    
+</div>
+</body>
+</html>
